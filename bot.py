e0c09bb54027828547681641b13c8cbb6ba394ca
evo <84800113+evo0616lution@users.noreply.github.com>
2022-01-19 22:53:19 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,3 +1,19 @@
 import nextcord as discord
 from nextcord.ext import commands
-scam_detector = commands.Bot(command_prefix="!")
+from pyjavaproperties import Properties
+p = Properties()
+p.load(open('bot.properties'))
+TOKEN = p['token']
+PREFIX = p['prefix']
+scam_detector = commands.Bot(command_prefix=PREFIX, help_command=None)
+
+@scam_detector.event
+async def on_message(msg):
+  f = open("list.txt", "r")
+  if msg.content in f.read():
+    member = msg.author
+    await member.kick(reason="Sent a scam link")
+  else:
+    return
+  
+scam_detector.run(TOKEN)  
